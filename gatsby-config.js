e5b53668c23890b5140f60f5a8aa0d2e75bcb40e
avergnaud <a.vergnaud@gmail.com>
2020-12-20 17:40:56 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: "https://ce-vin-ci-cms.herokuapp.com",
+        contentTypes: [
+          "restaurant",
+          "category",
+        ],
+        queryLimit: 1000,
+      },
+    }    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
